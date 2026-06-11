b2a14a74e44ff514a9336dd47c46011451dbb097
João Nakano <138939556+joaonakano@users.noreply.github.com>
2026-06-11 20:02:48 -0300

    feat: accounts page first look

--- src/renderer/src/components/nav-button.tsx
+++ src/renderer/src/components/nav-button.tsx
@@ -20,7 +20,7 @@ export function NavButton({
             size="sm"
             variant="outline"
             className={cn(
-                "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition",
+                "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition p-4",
                 isActive ? "bg-white/10 text-white" : "bg-transparent",
             )}
         >
--- src/renderer/src/pages/accounts/AccountsPage.tsx
+++ src/renderer/src/pages/accounts/AccountsPage.tsx
@@ -1,10 +1,28 @@
 import { DashboardLayout } from "../dashboard/components/layout";
 
+import { useNewAccount } from "./hooks/use-new-account";
+
+import { Button } from "@renderer/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@renderer/components/ui/card";
+import { Plus } from "lucide-react";
+
 export default function AccountsPage() {
+    const newAccount = useNewAccount()
+    
     return (
         <DashboardLayout>
-            <div>
-                Hello World
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-10">
+                <Card>
+                    <CardHeader className="flex flex-col gap-y-2 lg:flex-row lg:items-center lg:justify-between w-full">
+                        <CardTitle className="text-xl font-bold line-clamp-1">
+                            Accounts Page
+                        </CardTitle>
+                        <Button onClick={newAccount.onOpen} size="sm" className="w-full lg:w-auto lg:ml-auto p-4">
+                            <Plus className="size-4 mr-2" />
+                            Add new
+                        </Button>
+                    </CardHeader>
+                </Card>
             </div>
         </DashboardLayout>
     )
